6bceecbf37c91296c4cc94e596ce537f5a7f81c5
정소민 <yjk70702002@gmail.com>
2024-01-12 01:35:19 +0900

    JPA 기반 테스트

--- src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -2,20 +2,25 @@ package study.datajpa.repository;
 
 import org.assertj.core.api.Assertions;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.domain.Member;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
 @Transactional
 @Rollback(value = false)
-class MemberJpaRepositoryTest {
+public class MemberJpaRepositoryTest {
 
-    @Autowired MemberJpaRepository memberJpaRepository;
+    @Autowired
+    private MemberJpaRepository memberJpaRepository;
 
     @Test
     public void testMember() {
@@ -26,6 +31,8 @@ class MemberJpaRepositoryTest {
 
         Assertions.assertThat(findMember.getId()).isEqualTo(member.getId());
         Assertions.assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+
+        Assertions.assertThat(findMember).isEqualTo(member);
     }
 
 }
