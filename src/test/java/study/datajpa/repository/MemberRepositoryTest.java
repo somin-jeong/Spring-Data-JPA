f2b4376b9af2df00b01532d44e659337c3f1f6e8
정소민 <yjk70702002@gmail.com>
2024-01-26 00:42:29 +0900

    스프링 데이터 JPA 페이징과 정렬 - Slice

--- src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.Sort;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
@@ -181,21 +182,19 @@ public class MemberRepositoryTest {
         PageRequest pageRequest = PageRequest.of(0, 3, Sort.by(Sort.Direction.DESC, "username"));
 
         //when
-        Page<Member> page = memberRepository.findByAge(age, pageRequest);
+        Slice<Member> page = memberRepository.findByAge(age, pageRequest);
 
         //then
         List<Member> content = page.getContent(); // 조회된 데이터
-        long totalElements = page.getTotalElements();
 
         for (Member member : content) {
             System.out.println("member = " + member);
         }
-        System.out.println("totalElements = " + totalElements);
 
         assertThat(content.size()).isEqualTo(3); // 조회된 데이터 수
-        assertThat(page.getTotalElements()).isEqualTo(5); // 전체 데이터 수
+//        assertThat(page.getTotalElements()).isEqualTo(5); // 전체 데이터 수
         assertThat(page.getNumber()).isEqualTo(0); // 페이지 번호
-        assertThat(page.getTotalPages()).isEqualTo(2); // 전체 페이지 번호
+//        assertThat(page.getTotalPages()).isEqualTo(2); // 전체 페이지 번호
         assertThat(page.isFirst()).isTrue(); // 첫번째 항목인가?
         assertThat(page.hasNext()).isTrue(); // 다음 페이지가 있는가?
     }
