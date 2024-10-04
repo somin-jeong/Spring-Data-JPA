66ce74a3d78b32489fe36e43ab96d2af060d1c07
정소민 <yjk70702002@gmail.com>
2024-10-04 20:19:39 +0900

    Auditing 순수 JPA 사용

--- src/test/java/study/datajpa/domain/MemberTest.java
+++ src/test/java/study/datajpa/domain/MemberTest.java
@@ -4,21 +4,25 @@ import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.repository.MemberRepository;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+@RunWith(SpringRunner.class)  // JUnit5랑 스프링 사용하면 없어도 됨
 @SpringBootTest
 @Transactional
 @Rollback(value = false)
 public class MemberTest {
     @PersistenceContext
     EntityManager em;
+
+    @Autowired
+    MemberRepository memberRepository;
 
     @Test
     public void testEntity() {
@@ -51,4 +55,29 @@ public class MemberTest {
             System.out.println("-> member.team=" + member.getTeam());
         }
     }
+
+    @Test
+    public void jpaEventBaseEntity() throws Exception {
+        //given
+        Member member = new Member("member1");
+        memberRepository.save(member); // @PrePersist
+
+        Thread.sleep(100);
+        member.setUsername("member2");
+
+        System.out.println("=====================");
+
+        em.flush(); // @PreUpdate
+        em.clear();
+
+        System.out.println("=====================");
+
+        //when
+        Member findMember = memberRepository.findById(member.getId()).get();
+
+        //then
+        System.out.println("findMember.createdDate = " + findMember.getCreatedDate());
+        System.out.println("findMember.updatedDate = " + findMember.getUpdatedDate());
+    }
+
 }
